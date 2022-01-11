b498f91f045184113a31bbe28bf19d0798c07c28
SEJUN JANG <canopus0901@gmail.com>
2022-01-11 19:16:46 +0800

    Time: 70 ms (37.03%), Space: 13.4 MB (67.27%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        n = len(x)
+        for i in range(n // 2):
+            if x[i] != x[n - i - 1]:
+                return False
+        return True
