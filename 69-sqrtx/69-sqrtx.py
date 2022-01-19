e8d7586ccf2968bb13c02015e79b39f3ee9c864a
SEJUN JANG <canopus0901@gmail.com>
2022-01-19 20:38:53 +0800

    Time: 29 ms (51.66%), Space: 13.3 MB (65.80%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,10 @@
+import math
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        answer = int(math.sqrt(x)) 
+        return answer
