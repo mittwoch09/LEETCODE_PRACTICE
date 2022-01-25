352115406a1767900c781c072f97a8ec765112ab
SEJUN JANG <canopus0901@gmail.com>
2022-01-25 23:10:51 +0800

    Time: 24 ms (53.40%), Space: 13.6 MB (34.44%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -6,17 +6,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        stack = deque()
-        
-        for ch in s:
-            if ch == '(':
-                stack.append(')')
-            elif ch == '{':
-                stack.append('}')
-            elif ch == '[':
-                stack.append(']')
-            else:
-                if not stack or stack.pop() != ch:
-                    return False
-                
-        return not stack # return False if stack else True
+        stack = []                                              
+        brkt = {')': '(', '}': '{', ']': '['}                   
+        brkt_open = brkt.values()                              
+
+        for ch in s:                                           
+            if ch in brkt_open:                                 
+                stack.append(ch)                                
+            else:                                               
+                if len(stack) > 0 and stack[-1] == brkt[ch]:    
+                    stack.pop()                                 
+                else:                                           
+                    return False                                
+        return len(stack) == 0                                  
