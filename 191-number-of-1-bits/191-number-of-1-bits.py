a75dfd9d011b28482fa192aed1f3f6421b2e77dc
SEJUN JANG <canopus0901@gmail.com>
2022-03-24 22:49:53 +0800

    Time: 36 ms (18.05%), Space: 13.2 MB (97.09%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        while n > 0:
+            res += n % 2
+            n = n >> 1
+        return res
+
+    
+        '''
+        res = 0
+        while n:
+            n &= (n -1)
+            res += 1
+        return res
+        '''
